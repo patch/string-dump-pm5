42446acde6e893834c7aaef25d6014252884957f
Nick Patch <patch@cpan.org>
2011-12-05 08:09:42 -0500

    new project

---
+++ lib/String/Dumper.pm
@@ -0,0 +1,52 @@
+package String::Dumper;
+
+use 5.006;
+use strict;
+use warnings;
+use parent 'Exporter';
+
+our $VERSION = '0.01';
+our @EXPORT  = qw( dump_string );
+
+sub dump_string { }
+
+1;
+
+__END__
+
+=head1 NAME
+
+String::Dumper - Dump strings of characters or bytes for printing and debugging
+
+=head1 VERSION
+
+This document describes String::Dumper version 0.01.
+
+=head1 SYNOPSIS
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 SEE ALSO
+
+=over
+
+=item * ...
+
+=back
+
+=head1 AUTHOR
+
+Nick Patch <patch@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2011 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
